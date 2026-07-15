f617f8c8a975e5f0128c77742748cf4fdda9dd85
mkhan026 <itxmemahnoor56@gmail.com>
2026-07-15 16:41:03 +0500

    Add files via upload
    Day 7 journey of Backend development with ProSensia!
    Like it!

---
+++ first.js
@@ -0,0 +1,15 @@
+alert("Welcome Back Mahnoor!");
+
+
+const student= {
+    name : "Mahnoor Khan",
+    day : 7,
+    progress : 100,
+    islearning : true,
+    feedback:undefined
+};
+console.log(student);
+
+let inputday= prompt("Day7");
+let daysleft= 30-(inputday);
+console.log("daysleft",daysleft);
